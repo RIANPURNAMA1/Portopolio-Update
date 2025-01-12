5f90cdb989b1e2087256d8ed477a9a33b96b3f10
Rian Purnama <rianprnma7@gmail.com>
2025-01-12 18:18:15 +0700

    perbaikan

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 
-import './App.css'
-import AOS from 'aos';
 import 'aos/dist/aos.css';
 import About from './components/About'
 import ContactMe from './components/ContactMe'
@@ -12,10 +10,7 @@ import Services from './components/Services'
 import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
+
 
   return (
    <>
