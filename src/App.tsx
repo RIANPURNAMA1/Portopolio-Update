18873fabb78eaa6240cb0aa2dace1ba8c856f524
Rian Purnama <rianprnma7@gmail.com>
2025-01-12 18:44:09 +0700

    perbaikan aos

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,15 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Project from './components/Project'
 import Services from './components/Services'
+import AOS from  'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
-
-function App() {
-
+const App:React.FC = ()=> {
+  
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   return (
    <>
